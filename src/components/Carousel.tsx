69853b41759b1f35a6d54f231bbd8ecc9f281e18
Teemu <miettinen.dev@gmail.com>
2024-01-24 21:22:10 +0200

    Updated carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,32 +1,71 @@
 import React from "react";
-import { Box, Button, Carousel, Grommet, Text } from "grommet";
+import { Box, Button, Carousel, Grommet, Text, ThemeType } from "grommet";
+import { navigate } from "gatsby";
+import {
+  Attraction,
+  Car,
+  RadialSelected,
+  Rewind,
+  FastForward,
+  TreeOption,
+} from "grommet-icons";
+
+const customTheme: ThemeType = {
+  carousel: {
+    animation: {
+      duration: 500,
+    },
+    icons: {
+      color: "cyan",
+      current: RadialSelected,
+      next: FastForward,
+      previous: Rewind,
+    },
+  },
+};
+
+const blogPage = "/posts/";
 
 export const InteractiveSlides = () => (
-  <Grommet>
+  <Grommet theme={customTheme}>
     <Box align="center" pad="large">
       <Carousel
-        controls="arrows"
+        controls="selectors"
         height="medium"
         width="xlarge" /*play={5000}*/
       >
-        <Box fill align="center" justify="center" gap="small">
-          <Text weight="bold" size="xlarge">
-            Slide 1
-          </Text>
-          <Button label="Button" />
-        </Box>
         <Box
           fill
           align="center"
           justify="center"
-          background="light-3"
           gap="small"
+          // background={{
+          //   image: "url(/images/carousel_border.png)",
+          //   opacity: "strong",
+          // }}
         >
+          <Text weight="bold" size="xxlarge">
+            Project: Star Defender
+          </Text>
           <Text weight="bold" size="xlarge">
-            Slide 2
+            Star defender is arcade-like top-down shooter. Inspired from old
+            Amiga game called Star Control.
           </Text>
           <Button label="Button" />
-          <Button label="Button" />
+        </Box>
+        <Box fill align="center" justify="center" gap="small">
+          <Text weight="bold" size="xxlarge">
+            Devlog
+          </Text>
+          <Text weight="bold" size="xlarge">
+            Follow the development progress!
+          </Text>
+          <Button
+            size="xlarge"
+            gap="medium"
+            onClick={() => navigate(blogPage)}
+            label="To the Devlog!"
+          />
         </Box>
         <Box
           fill
