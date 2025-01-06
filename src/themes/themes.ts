97e12bd5e691fa3b995463215ffb94685d0c7a2d
Teemu <miettinen.dev@gmail.com>
2025-01-06 18:24:18 +0200

    Blog post 14 started.

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,9 +1,12 @@
 export const mainTheme = {
-    global: {
-      colors: { main: "linear-gradient(326deg, rgba(14,14,14,1) 0%, rgba(0,53,125,1) 42%, rgba(23,23,23,1) 100%)", text: "white", background: "rgb(14,14,14)" },
-      
+  global: {
+    colors: {
+      main: "linear-gradient(326deg, rgba(14,14,14,1) 0%, rgba(0,53,125,1) 42%, rgba(23,23,23,1) 100%)",
+      text: "white",
+      background: "rgb(14,14,14)",
     },
-  }
+  },
+};
 
-  //  colors: { main: "linear-gradient(#474b4F,#222629)", text: "white", background:"#6B6E70" },
-  // background: "#6B6E70"
+//  colors: { main: "linear-gradient(#474b4F,#222629)", text: "white", background:"#6B6E70" },
+// background: "#6B6E70"
